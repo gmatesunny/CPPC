7dbd5e8fa9140872b18f778928bd787640ba010a
Roshan Mehta <gmate.sunny@gmail.com>
2018-02-19 10:47:07 +0100

    Create revise_1.cpp

---
+++ CPP/revise_1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+    int imag;
+public:
+    Complex(): real(0), imag(0) {}
+    void read_data()
+    {
+        cout << "Enter real and imag value";
+        cin >> real >> imag;
+    }
+    void add_complex(Complex comp1, Complex comp2 )
+    {
+        real = comp1.real + comp2.real;
+        imag = comp1.imag + comp2.imag;
+    }
+
+    static Complex add_complex_return(Complex comp1, Complex comp2 )
+    {
+        Complex temp;
+        temp.real = comp1.real + comp2.real;
+        temp.imag = comp1.imag + comp2.imag;
+        return temp;
+    }
+
+    void display_sum()
+    {
+        cout << "sum " << real << ", i" << imag;
+    }
+
+};
+
+int main()
+{
+    Complex c1, c2, c3;
+    c1.read_data();
+    c2.read_data();
+
+    c3 = Complex::add_complex_return(c1, c2);
+
+    c3.display_sum();
+
+    return 0;
+}
+
