38d58babd5f6b650eb1213c7a160023d04bc6cfd
Roshan Mehta <gmate.sunny@gmail.com>
2018-08-23 14:16:04 +0200

    Update greatest_first.cpp

--- CPP/greatest_first.cpp
+++ CPP/greatest_first.cpp
@@ -5,19 +5,30 @@ Given A = [1, 2, 3], the function should return 4.
 Given A = [−1, −3], the function should return 1.
 */
 
+#include <iostream>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<int> &A) {
-    auto i = 1, j = 0;
+ int solution(vector<int> &A) {
+    auto i = 0, j = 0;
     sort(begin(A), end(A));
-    while(j < (int)A.size())
-    {
-        if(A[j]+1 < A[j+1])
-            break;
+    
+    if(A[(int)A.size()-1] < 1)
+        return 1;    
+            
+    while(A[j]< 1)
         j++;
-        i++;
+  
+    if(A[j] > 1)
+        return 1;
+
+  while(j < (int)A.size())
+    {         
+        if(j+1 < (int)A.size() && A[j+1] - A[j] > 1)
+            return A[j] + 1;
+        j++;
     }
   
-    return i;
+    return A[j-1] + 1;  
 }
