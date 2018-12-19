1ac050e9be0b54295f4fa23316cdeb7e22a465e9
Roshan Mehta <gmate.sunny@gmail.com>
2018-12-19 21:36:22 +0100

    Create 19Dec.cpp

---
+++ CPP/19Dec.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int32_t tc{0};
+  cin >> tc;
+	while(tc--)
+	{
+	    int32_t n{0}, d{0}, n1{0}, n2{0}, nValue{0}, j{2};
+    	cin >> n1 >> n2;
+    	cin >> n;
+    	d = n2-n1;
+        nValue = n1 + (n - 1)*d;
+    /*	if(n == 1)
+    	{
+    	    nValue = n1;
+    	}
+    	else
+    	{
+    	    nValue = n2;
+    	    while(j<n)
+        	{
+        	    j++;
+        	    nValue = nValue + d;
+        	}
+    	} */
+    	cout << nValue << endl;
+	}
+	return 0;
+}
